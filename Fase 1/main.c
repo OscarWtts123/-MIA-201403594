9de472a593ec1a755e4aded7b9bf8d73f5c42788
dacore <alidaryousef@gmail.com>
2016-07-31 15:43:41 -0600

    ya distingue comandos, inicio de reconocimiento de parámetros

--- Fase 1/main.c
+++ Fase 1/main.c
@@ -5,16 +5,44 @@
 
 int main()
 {
-    printf("Ingrese un comando:\n");
     char comando[256];
+    int longitud = 0;
+    printf("\t\t\t  Ingrese una instrucción:\n");
     scanf(" %[^\n]", comando);
+
+    if(strcmp(&comando[strlen(comando) -1],"\\") == 0)
+    {
+        comando[strlen(comando) -1] = '\0';
+        concatenarComandoMultilinea(comando);
+    }
+
 
     while(strcmp(comando,"exit") != 0)
     {
         leerComando(comando);
+        printf("\n\n\t\t\t  Ingrese una instrucción:\n");
         scanf(" %[^\n]", comando);
+        if(strcmp(&comando[strlen(comando) -1],"\\") == 0)
+        {
+            comando[strlen(comando) -1] = '\0';
+            concatenarComandoMultilinea(comando);
+        }
     }
     return 0;
+}
+
+void concatenarComandoMultilinea(char* comandoCompleto)
+{
+    char nuevaLinea[256];
+    scanf(" %[^\n]", nuevaLinea);
+    strcat(comandoCompleto, " ");
+    strcat(comandoCompleto, nuevaLinea);
+
+    if(strcmp(&nuevaLinea[strlen(nuevaLinea) -1],"\\") == 0)
+    {
+        comandoCompleto[strlen(comandoCompleto) -1] = '\0';
+        concatenarComandoMultilinea(comandoCompleto);
+    }
 }
 
 //este método convierte un string a su equivalente en minúscula
@@ -43,16 +71,30 @@ void leerComando(char* linea)
     comando = strtok(linea," ");                                            //separa el comando del resto de instrucciones
     aMinuscula(comando);
 
-    //caso 0: exec
-    //caso 1: mkdisk
-    //caso 2: rmdisk
-    //caso 3: fdisk
-    //caso 4: mount
-    //caso 5: unmount
+    //lista de parámetros de todos los comandos
+    char size[256];
+    char unit[256];
+    char path[256];
+    char name[256];
+    char type[256];
+    char fit[256];
+    char delete[256];
+
+    //casoComando 0: exec
+    //casoComando 1: mkdisk
+    //casoComando 2: rmdisk
+    //casoComando 3: fdisk
+    //casoComando 4: mount
+    //casoComando 5: unmount
 
     if(strcmp(comando,"exec") == 0)
     {
         casoComando = 0;
+        while(comando)                  //mientras aún tenga instrucciones
+        {
+            printf("%s\n", comando);
+            comando = strtok(NULL, " ");
+        }
     }
     else if(strcmp(comando,"mkdisk") == 0)
     {
@@ -70,16 +112,16 @@ void leerComando(char* linea)
     {
         casoComando = 4;
     }
-    else if(strcmp(comando,"unmount") == 0)
+    else if(strcmp(comando,"unmount:") == 0)
     {
         casoComando = 5;
     }
-
-    while(comando)                                                          //mientras tenga tokens
+    else
     {
-        printf("%s\n", comando);
-        comando = strtok(NULL, ":");
+        printf("****Error: El comando \"%s\" no es válido.****\n",comando);
     }
+
+
     printf("Acabó de leer los tokens y la línea original era: %s\n", lineaAux);
 }
 
